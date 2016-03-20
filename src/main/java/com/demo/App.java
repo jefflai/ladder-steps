8594c0ebffc67ef04ba7d52029b01c631cc0823c
jefflai <j3ffr3y@gmail.com>
2016-03-20 11:05:07 -0700

    fix formatting

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,37 +1,34 @@
 package com.demo;
 
 /**
- *  One ladder has 100 steps high, one rabbit can jump 1-4 steps each time, get the total possibilities the rabbit can jump.
- *  For example: 1,1,1,1,…1 (100 times of 1), 2,2,2,2 … (50 times of 2), etc.
+ * One ladder has 100 steps high, one rabbit can jump 1-4 steps each time, get
+ * the total possibilities the rabbit can jump. For example: 1,1,1,1,…1 (100
+ * times of 1), 2,2,2,2 … (50 times of 2), etc.
  */
 public class App {
 
-    public static int countPossibilities(int ladderStepsCount) {
-    	if (ladderStepsCount < 1) {
-    		throw new IllegalArgumentException("ladderStepsCount must be a postive integer");
-    	}
-    	
-    	return countPossibilitiesHelper(ladderStepsCount, 1) 
-    			+ countPossibilitiesHelper(ladderStepsCount, 2) 
-    			+ countPossibilitiesHelper(ladderStepsCount, 3) 
-    			+ countPossibilitiesHelper(ladderStepsCount, 4);
-    }
-    
-    public static int countPossibilitiesHelper(int ladderStepsRemaining, int stepSizeToTake) {
-    	if (ladderStepsRemaining >= stepSizeToTake) {
-    		ladderStepsRemaining -= stepSizeToTake;
-    	} else {
-    		return 0;
-    	}
-    	
-    	if (ladderStepsRemaining == 0) {
-    		return 1;
-    	}
-    	
-    	return countPossibilitiesHelper(ladderStepsRemaining, 1) 
-    			+ countPossibilitiesHelper(ladderStepsRemaining, 2) 
-    			+ countPossibilitiesHelper(ladderStepsRemaining, 3) 
-    			+ countPossibilitiesHelper(ladderStepsRemaining, 4);
-    }
-    
+	public static int countPossibilities(int ladderStepsCount) {
+		if (ladderStepsCount < 1) {
+			throw new IllegalArgumentException("ladderStepsCount must be a postive integer");
+		}
+
+		return countPossibilitiesHelper(ladderStepsCount, 1) + countPossibilitiesHelper(ladderStepsCount, 2)
+				+ countPossibilitiesHelper(ladderStepsCount, 3) + countPossibilitiesHelper(ladderStepsCount, 4);
+	}
+
+	public static int countPossibilitiesHelper(int ladderStepsRemaining, int stepSizeToTake) {
+		if (ladderStepsRemaining >= stepSizeToTake) {
+			ladderStepsRemaining -= stepSizeToTake;
+		} else {
+			return 0;
+		}
+
+		if (ladderStepsRemaining == 0) {
+			return 1;
+		}
+
+		return countPossibilitiesHelper(ladderStepsRemaining, 1) + countPossibilitiesHelper(ladderStepsRemaining, 2)
+				+ countPossibilitiesHelper(ladderStepsRemaining, 3) + countPossibilitiesHelper(ladderStepsRemaining, 4);
+	}
+
 }
--- src/test/java/com/demo/AppTest.java
+++ src/test/java/com/demo/AppTest.java
@@ -4,74 +4,74 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class AppTest {
-	
-	//	f(1) = 1
-	//	1	
+
+	// f(1) = 1
+	// 1
 	@Test
 	public void testInputOne() {
 		Assert.assertEquals(1, App.countPossibilities(1));
 	}
-	
-	//	f(2) = 2
-	//	1,1
+
+	// f(2) = 2
+	// 1,1
 	//
-	//	2
+	// 2
 	@Test
 	public void testInputTwo() {
 		Assert.assertEquals(2, App.countPossibilities(2));
 	}
 
-	//	f(3) = 4  
-	//	1,1,1
+	// f(3) = 4
+	// 1,1,1
 	//
-	//	2,1
-	//	1,2
+	// 2,1
+	// 1,2
 	//
-	//	3
+	// 3
 	@Test
 	public void testInputThree() {
 		Assert.assertEquals(4, App.countPossibilities(3));
 	}
-	
-	//	f(4) = 8
-	//	1,1,1,1
+
+	// f(4) = 8
+	// 1,1,1,1
 	//
-	//	2,1,1
-	//	1,2,1
-	//	1,1,2
+	// 2,1,1
+	// 1,2,1
+	// 1,1,2
 	//
-	//	3,1
-	//	1,3
+	// 3,1
+	// 1,3
 	//
-	//	4
+	// 4
 	//
-	//	2,2
+	// 2,2
 	@Test
 	public void testInputFour() {
 		Assert.assertEquals(8, App.countPossibilities(4));
 	}
-	
-	//	f(5) = 15
-	//	1,1,1,1,1
+
+	// f(5) = 15
+	// 1,1,1,1,1
 	//
-	//	2,1,1,1
-	//	1,2,1,1
-	//	1,1,2,1
-	//	1,1,1,2
+	// 2,1,1,1
+	// 1,2,1,1
+	// 1,1,2,1
+	// 1,1,1,2
 	//
-	//	2,2,1
-	//	2,1,2
-	//	1,2,2
+	// 2,2,1
+	// 2,1,2
+	// 1,2,2
 	//
-	//	3,1,1
-	//	1,3,1
-	//	1,1,3
+	// 3,1,1
+	// 1,3,1
+	// 1,1,3
 	//
-	//	3,2
-	//	2,3
+	// 3,2
+	// 2,3
 	//
-	//	4,1
-	//	1,4
+	// 4,1
+	// 1,4
 	@Test
 	public void testInputFive() {
 		Assert.assertEquals(15, App.countPossibilities(5));
